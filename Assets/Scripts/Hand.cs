70daecf545a23e719579f9c95da277bd3efe431f
ThereExistsX <ThereExistsX@users.noreply.github.com>
2019-05-06 18:59:17 -0400

    Move all the grab stuff into one file

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class Hand : MonoBehaviour {
+    public XRNode NodeType;
+
+    void Update(){
+        transform.localPosition = InputTracking.GetLocalPosition(NodeType);
+        transform.localRotation = InputTracking.GetLocalRotation(NodeType);
+    }
+
+    void OnTriggerStay(Collider other) {
+        if(other.gameObject.tag == "Grabbable"){
+            if(Input.GetButton("Grab")){
+                print(InputTracking.GetLocalPosition(NodeType));
+                other.transform.position = gameObject.transform.position;
+            }
+        }
+    }
+}
