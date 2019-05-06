438379d421ce14f1f41f71e2630bb5308279faf9
ThereExistsX <ThereExistsX@users.noreply.github.com>
2019-05-06 14:45:26 -0400

    Groundwork for grabbing

---
+++ Assets/Scripts/Grab.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grab : MonoBehaviour
+{
+    void Update(){
+        if(Input.GetButton("Grab")){
+            print("clicky");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Grabbable"){
+            print("I can grab this.");
+        }
+    }
+}
